cb77234285e84e61a7d4a9320b2b075d0649dd7a
seosd97 <seosd97@gmail.com>
2025-06-27 17:01:47 +0900

    add override type for promise

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
+import { FlashMemoOptions } from "../types";
+
 export function flashMemo<T = unknown, R = unknown>(
   fn: (...args: T[]) => Promise<R>,
-  delay = 1000
+  options?: FlashMemoOptions
+): (...args: T[]) => Promise<R>;
+
+export function flashMemo<T = unknown, R = unknown>(
+  fn: (...args: T[]) => R,
+  options?: FlashMemoOptions
+): (...args: T[]) => R;
+
+export function flashMemo<T = unknown, R = unknown>(
+  fn: (...args: T[]) => Promise<R>,
+  options?: FlashMemoOptions
 ): (...args: T[]) => Promise<R> {
   let lastCall: number = 0;
   let lastResult: Promise<R>;
 
-  if (delay <= 0) {
+  const { cacheTime = 1000 } = options || {};
+
+  if (cacheTime <= 0) {
     return fn;
   }
 
   return async function (...args: T[]) {
     const now = Date.now();
 
-    if (now - lastCall < delay) {
+    if (now - lastCall < cacheTime) {
       return lastResult;
     }
 
